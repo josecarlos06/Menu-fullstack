59bb8d1a5a5f1665037d0699fd56a6bd28d40117
josecarlos06 <carlosmedina.vasq06@gmail.com>
2023-04-29 22:10:26 -0600

    add Layout

---
+++ src/Layout/Layout.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+const Layout = ({children,title}) => {
+  return (
+   <>
+      <Head>
+         <title>{title}</title>
+      </Head>
+      <main>
+         {children}
+      </main>
+   </>
+  )
+}
+
+export default Layout
