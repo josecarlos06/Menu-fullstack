551aa16b0ac82d5e8c541493e53fd50dda576de2
josecarlos06 <carlosmedina.vasq06@gmail.com>
2023-04-29 22:23:51 -0600

    prisma

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { MenuProvider } from '@/context/MenuProvider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <MenuProvider>
+         <Component {...pageProps} />
+      </MenuProvider>
+  )
 }
